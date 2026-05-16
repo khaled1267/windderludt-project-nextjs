4fca260974eb0b6a54406f2be0894a146843c76c
khaled1267 <khaledkhan1267@gmail.com>
2026-05-16 21:17:11 +0600

    fontend

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,7 +2,7 @@ import { jwtClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "https://wanderlush-project-server.vercel.app",
+  // baseURL: "https://wanderlush-project-server.vercel.app",
   plugins: [jwtClient()],
 });
 
